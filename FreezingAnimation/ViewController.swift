405a9cf804a1a19dc927c5548ba2dec6f59001b4
Liliya <lilyeka@gmail.com>
2019-06-13 14:13:33 +0500

    files

--- FreezingAnimation/ViewController.swift
+++ FreezingAnimation/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var shape: CAShapeLayer!
+    
+    var myView: UIView = {
+        var view = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        return view
+    }()
+    
+    var slider: UISlider = {
+        var slider = UISlider(frame: CGRect(x: 100, y: 250, width: 150, height: 50))
+        slider.addTarget(self, action: #selector(sliderAction), for: .valueChanged)
+        return slider
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.addSubview(myView)
+        view.addSubview(slider)
+        initShape()
     }
-
-
+    
+    func initShape() {
+        let shape = CAShapeLayer()
+        shape.frame = myView.bounds
+        myView.layer.addSublayer(shape)
+        shape.fillColor = UIColor.clear.cgColor
+        shape.strokeColor = UIColor.red.cgColor
+        
+        let path = CGPath(rect: shape.bounds, transform: nil)
+        shape.path = path
+        
+        let path2 = CGPath(ellipseIn: shape.bounds, transform: nil)
+        
+        let ba = CABasicAnimation(keyPath: #keyPath(CAShapeLayer.path))
+        ba.duration = 1
+        ba.fromValue = path
+        ba.toValue = path2
+        shape.speed = 0
+        shape.timeOffset = 0
+        shape.add(ba, forKey: nil)
+        self.shape = shape
+    }
+    
+    @objc func sliderAction() {
+        self.shape.timeOffset = Double(slider.value)
+    }
 }
 
